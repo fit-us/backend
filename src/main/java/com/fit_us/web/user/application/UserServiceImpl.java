1e23bba286c499061658d32dcfe99e0c9dab8dd5
KNU-K <knukang334@gmail.com>
2025-03-18 00:22:47 +0900

    feat : 사용자 서비스 구현체 추가 (생성, 삭제 로직)

---
+++ src/main/java/com/fit_us/web/user/application/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.fit_us.web.user.application;
+
+import com.fit_us.web.user.application.dto.CreateUserCommand;
+import com.fit_us.web.user.domain.Profile;
+import com.fit_us.web.user.domain.User;
+import com.fit_us.web.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public User create(CreateUserCommand command) {
+        User newUser = createUser(command);
+        Profile newProfile = createProfile(command.getProfile());
+        newUser.addProfile(newProfile);
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    public User update(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
+    private User createUser(CreateUserCommand command) {
+        if(command == null){
+            throw new IllegalArgumentException("CreateUserCommand cannot be null.");
+        }
+        return User.create(
+                command.getName(),
+                command.getNickname(),
+                command.getOauthId(),
+                command.getProvider(),
+                command.getEmail()
+        );
+    }
+
+    private Profile createProfile(CreateUserCommand.ProfileInfo command) {
+        if(command == null){
+            throw new  IllegalArgumentException("Profile information cannot be null.");
+        }
+        return Profile.create(
+                command.getProfileImageUrl(),
+                command.getBirthDate(),
+                command.getGender(),
+                command.getBloodType()
+        );
+    }
+}
